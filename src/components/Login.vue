7fe04092bf9649e4ef8e3de49dd2e2b8c53c32fc
knicked <kn1ck3d@gmail.com>
2018-04-06 19:53:23 +0200

    Added login page

---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-form v-model="valid" ref="form" lazy-validation>
+    <v-text-field
+      label="Username"
+      v-model="username"
+      :rules="usernameRules"
+      required
+    ></v-text-field>
+    <v-text-field
+      label="Password"
+      v-model="password"
+      type="password"
+      :rules="passwordRules"
+      required
+    ></v-text-field>
+    <v-btn
+      @click="login"
+      :disabled="!valid"
+    >
+    Login
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      valid: true,
+      username: '',
+      usernameRules: [
+        v => !!v || 'Username is required'
+      ],
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required'
+      ]
+    }
+  },
+  methods: {
+    login () {
+      if (this.$refs.form.validate()) {
+        // send an API login request with the username and the password
+      }
+    }
+  }
+}
+</script>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -28,6 +28,10 @@ export default {
         {
           name: 'Home',
           to: '/'
+        },
+        {
+          name: 'Login',
+          to: '/login'
         }
       ]
     }
