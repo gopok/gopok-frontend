c903d14489fd00f9cfb6da7221cc3d330ccaafa0
xdk78 <xdk78@interia.pl>
2018-04-07 13:20:16 +0200

    Add Feed placeholder

---
+++ src/components/Feed.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+<v-flex xs12 sm6 offset-sm3 v-for="i in 10" :key="i">
+
+      <v-card class="my-1" >
+        <v-card-title>
+          <div>
+            <span class="grey--text">Test Header {{i}}</span><br>
+            <span>test</span><br>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat color="accent">Share</v-btn>
+        </v-card-actions>
+      </v-card>
+
+    </v-flex>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Feed',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Feed></Feed>
+  </div>
+</template>
+
+<script>
+import Feed from '@/components/Feed'
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  },
+  components: { Feed }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
